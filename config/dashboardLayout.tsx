dcf37dc724edd9e266365dbee28ac6893dd8f05f
Thirumurugan <90082466+Thirumurugan-KS@users.noreply.github.com>
2025-02-19 21:45:38 +0530

    Added Layout

--- config/dashboardLayout.tsx
+++ config/dashboardLayout.tsx
@@ -5,41 +5,45 @@ import { FaRegBuilding, FaChartLine } from "react-icons/fa";
 import { TiMessageTyping } from "react-icons/ti";
 import { PiSignOut } from "react-icons/pi";
 
+const iconStyle = {
+    display: 'inline',
+}
+
 const layoutComponents = [
     {
         id: 1,
         title: "Dashboard",
-        icon: <RxDashboard size={20}/>,
+        icon: <RxDashboard size={20} style={iconStyle}/>,
     },
     {
         id: 2,
         title: "Notification",
-        icon: <CiBellOn size={20}/>,
+        icon: <CiBellOn size={20} style={iconStyle}/>,
     },
     {
         id: 3,
         title: "Dealers",
-        icon: <IoBodyOutline size={20}/>,
+        icon: <IoBodyOutline size={20} style={iconStyle}/>,
     },
     {
         id: 4,
         title: "Customer management",
-        icon: <FaRegBuilding size={20}/>,
+        icon: <FaRegBuilding size={20} style={iconStyle}/>,
     },
     {
         id: 5,
         title: "Report",
-        icon: <FaChartLine size={20}/>,
+        icon: <FaChartLine size={20} style={iconStyle}/>,
     },
     {
         id: 6,
         title: "Grievance",
-        icon: <TiMessageTyping size={20}/>,
+        icon: <TiMessageTyping size={20} style={iconStyle}/>,
     },
     {
         id: 7,
         title: "Sign out",
-        icon: <PiSignOut size={20}/>,
+        icon: <PiSignOut size={20} style={iconStyle}/>,
     },
 ]
 
